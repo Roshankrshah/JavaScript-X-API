9f67d94c7fac11b254b0231ebc861ffc22c14f83
Roshan <shahroshan0720@gmail.com>
2023-07-15 19:07:02 +0530

    setup fetch method

--- Dictionay App/app.js
+++ Dictionay App/app.js
@@ -4,5 +4,15 @@ const btn = document.getElementById('search-btn');
 
 btn.addEventListener('click',(e)=>{
     e.preventDefault();
-    console.log("i m searching");
+    let inpWord = document.getElementById('inp-word').value;
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inpWord}`)
+    .then((response) => response.json())
+    .then((data)=>{
+        
+    })
+    .catch(()=>{
+        console.log("fattu");
+    })
+    
+    console.log(inpWord);
 })
