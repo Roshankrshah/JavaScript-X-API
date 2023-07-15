4768da8a311cecbe51d3bc76588f4be055f4c512
Roshan <shahroshan0720@gmail.com>
2023-07-15 18:49:59 +0530

    initial setup of app.js

---
+++ Dictionay App/app.js
@@ -0,0 +1,8 @@
+const result = document.getElementById('result');
+const sound = document.getElementById('sound');
+const btn = document.getElementById('search-btn');
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    console.log("i m searching");
+})
