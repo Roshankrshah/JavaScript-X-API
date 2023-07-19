c37b41a201725753d3434a57a8f53ac782a4f035
Roshan <shahroshan0720@gmail.com>
2023-07-19 22:04:14 +0530

    add various endpoints

--- Recipe App/app.js
+++ Recipe App/app.js
@@ -4,6 +4,64 @@ const recipeContainer = document.querySelector('.recipe-container');
 const recipeDetailsContent = document.querySelector('.recipe-details-content');
 const recipeCloseBtn = document.querySelector('.recipe-close-btn');
 
+const btnContainer = document.querySelector('.btn-container');
+const btns = document.querySelectorAll('.tab-btn');
+
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let searchBy = 'name';
+
+const recipeDetails = (recipes) => {
+    recipes.meals.forEach((recipe) => {
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('recipe');
+        recipeDiv.innerHTML = `
+            <img src="${recipe.strMealThumb}">
+            <h3>${recipe.strMeal}</h3>
+            <p><span>${recipe.strArea}</span> Dish</p>
+            <p>Belongs to <span>${recipe.strCategory}</span> Category</p>`
+
+        const button = document.createElement('button');
+        button.textContent = "View Recipe";
+        recipeDiv.appendChild(button);
+
+        button.addEventListener('click', () => {
+            openRecipeModal(recipe)
+        })
+
+        recipeContainer.appendChild(recipeDiv);
+    })
+}
+
+const recipeListDetails = (recipes) => {
+    recipes.meals.forEach((recipe) => {
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('recipe');
+        recipeDiv.innerHTML = `
+            <img src="${recipe.strMealThumb}">
+            <h3>${recipe.strMeal}</h3>`
+
+        const button = document.createElement('button');
+        button.textContent = "View Recipe";
+        recipeDiv.appendChild(button);
+        recipeContainer.appendChild(recipeDiv);
+
+        button.addEventListener('click', () => {
+            console.log("Mia khalifa")
+            url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipe.idMeal}`;
+            fetch(url).then((resp) => resp.json())
+                .then((recipeItem) => {
+                    recipeItem.meals.forEach((recipe) => {
+                        openRecipeModal(recipe);
+                    })
+                })
+                .catch(() => {
+                    recipeContainer.innerHTML = "<h1>Error in fetching Recipe...</h1>";
+                })
+        })
+    })
+
+}
+
 const fetchRecipe = async (query) => {
     recipeContainer.innerHTML = `
         <div id="content" class="row">
@@ -14,35 +72,20 @@ const fetchRecipe = async (query) => {
         </div>`;
 
     try {
-        let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+        let response = await fetch(url + `${query}`);
         let recipes = await response.json();
 
         recipeContainer.innerHTML = "";
-        recipes.meals.forEach((recipe) => {
-
-            const recipeDiv = document.createElement('div');
-            recipeDiv.classList.add('recipe');
-            recipeDiv.innerHTML = `
-                <img src="${recipe.strMealThumb}">
-                <h3>${recipe.strMeal}</h3>
-                <p><span>${recipe.strArea}</span> Dish</p>
-                <p>Belongs to <span>${recipe.strCategory}</span> Category</p>`
-
-            const button = document.createElement('button');
-            button.textContent = "View Recipe";
-            recipeDiv.appendChild(button);
-
-            button.addEventListener('click', () => {
-                openRecipeModal(recipe)
-            })
-
-            recipeContainer.appendChild(recipeDiv);
-        })
+        if (searchBy == 'name') {
+            recipeDetails(recipes)
+        } else {
+            recipeListDetails(recipes);
+        }
     }
-    catch{
+    catch {
         recipeContainer.innerHTML = "<h1>Error in fetching Recipe...</h1>";
     }
-    
+
 };
 
 recipeCloseBtn.addEventListener('click', () => {
@@ -63,6 +106,7 @@ const fectchIngredients = (recipe) => {
     }
     return ingredientList;
 }
+
 const openRecipeModal = (recipe) => {
     recipeDetailsContent.innerHTML = `
         <h2 class="recipename">${recipe.strMeal}</h2>
@@ -78,13 +122,34 @@ const openRecipeModal = (recipe) => {
     recipeDetailsContent.parentElement.style.display = 'block';
 }
 
-
-
 searchBtn.addEventListener('click', () => {
     let query = searchBox.value.trim();
-    if(!query){
+    if (!query) {
         recipeContainer.innerHTML = "<h1>Provie Any Recipe Name...</h1>";
         return;
     }
     fetchRecipe(query);
 })
+
+btnContainer.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    console.log(id);
+    if (id == 'name') {
+        url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+        searchBy = 'name';
+        searchBox.value= "";
+        searchBox.placeholder = "Search Recipe By Name...";
+    } else if (id == 'area') {
+        url = "https://www.themealdb.com/api/json/v1/1/filter.php?a=";
+        searchBy = 'area';
+        searchBox.value= "";
+        searchBox.placeholder = "Search Recipe By Area e.g. Indian";
+    } else if (id == 'category') {
+        url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+        searchBy = 'category';
+        searchBox.value= "";
+        searchBox.placeholder = "Search Recipe By Category e.g. Seafood";
+    }
+});
+
+
