e725ac6d71a33a29f34591d86120654f4138bf18
Roshan <shahroshan0720@gmail.com>
2023-07-28 12:48:10 +0530

    error handling

--- Image Search App/app.js
+++ Image Search App/app.js
@@ -9,23 +9,30 @@ const accessKey = "DKIZnX2ZmwYykmKkAoDfJXQbO2r4_aJxfD3ypjZ0iRQ";
 let page = 1;
 
 async function displayImages() {
-    const query = inputEle.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${accessKey}`;
-    try {
 
-        if(page == 1){
+    try {
+        const query = inputEle.value;
+        const url = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${accessKey}`;
+        if (!query) {
+            throw 'Empty';
+        }
+        if (page == 1) {
             searchResults.innerHTML = `<div class="loading"></div>`;
         }
 
         const resp = await fetch(url);
         const data = await resp.json();
+
         const results = data.results;
-        
-        if(page == 1){
+
+        if (results.length == 0)
+            throw 'Not Found';
+
+        if (page == 1) {
             searchResults.innerHTML = ``;
         }
 
-        results.map((result)=>{
+        results.map((result) => {
             const imageWrapper = document.createElement('div');
             imageWrapper.classList.add("result");
 
@@ -39,26 +46,30 @@ async function displayImages() {
             imageWrapper.appendChild(image);
             imageWrapper.appendChild(imageLink);
             searchResults.appendChild(imageWrapper);
-            
+
         })
         page++;
-        if(page>1){
+        if (page > 1) {
             showMoreBtn.style.display = 'block';
         }
 
-    }catch(err){
-        
+    } catch (err) {
+        if (err == 'Empty')
+            searchResults.innerHTML = `<h2>Please Provide Input Value</h2>`;
+        else if (err == 'Not Found')
+            searchResults.innerHTML = `<h2>Not Found</h2>`;
+        showMoreBtn.style.display = 'none';
     }
 }
 
-formEle.addEventListener('submit',(e)=>{
+formEle.addEventListener('submit', (e) => {
     e.preventDefault();
-    page=1;
+    page = 1;
     displayImages();
 });
 
 
 
-showMoreBtn.addEventListener('click',()=>{
+showMoreBtn.addEventListener('click', () => {
     displayImages();
 })
